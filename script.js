479965291bfd1085e213e8819837692b5e511d86
Sheila Kerber <sheilask.bio@gmail.com>
2020-06-09 14:55:49 -0300

    corrige formatacao

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ var displaySettingT1 = turma1.style.display;
 var displaySettingT2 = turma2.style.display;
 
 function notasTurma1() {
-    //resultadoTurma.innerHTML = turma1.value
     turma1.style.display = 'block'
     turma2.style.display = 'none'
 }
 
 function notasTurma2() {
-    //resultadoTurma.innerHTML = turma2.value
     turma2.style.display = 'block'
     turma1.style.display = 'none'
 }
